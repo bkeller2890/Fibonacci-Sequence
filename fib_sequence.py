aa01c449c638acc0430d910384ca90622287a474
Benjamin A Keller <107152820+bkeller2890@users.noreply.github.com>
2023-01-06 11:10:08 -0600

    Create fib_sequence.py
    main program

---
+++ fib_sequence.py
@@ -0,0 +1,13 @@
+#Benjamin Keller
+#Fibonacci Sequence 
+
+number = int(input()) 
+
+def fib_sequence(x)
+  if x in {0,1}: 
+    return x
+  else: 
+    return fib_sequence(x-1)+fib_sequence(x-2) 
+
+for i in range(number): 
+  print(fib_sequence(i)
